0531ae422798b5f318c66e7b66c06f6d3dffe1ec
Rohan <127836623+terminalrohan@users.noreply.github.com>
2025-06-04 12:59:31 +0530

    Update apiPaths.js

--- FRONTEND/interview-prep-ai/src/utils/apiPaths.js
+++ FRONTEND/interview-prep-ai/src/utils/apiPaths.js
@@ -1,31 +1,31 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const BASE_URL = "https://interview-prep-wxp4.onrender.com/api";
 
 export const API_PATHS = {
   AUTH: {
-    REGISTER: `${BASE_URL}/api/auth/register`, // Signup
-    LOGIN: `${BASE_URL}/api/auth/login`, // Authenticate user & return JWT token
-    GET_PROFILE: `${BASE_URL}/api/auth/profile`, // Get logged-in user details
+    REGISTER: `/auth/register`, // Signup
+    LOGIN: `/auth/login`, // Authenticate user & return JWT token
+    GET_PROFILE: `/auth/profile`, // Get logged-in user details
   },
 
   IMAGE: {
-    UPLOAD_IMAGE: `${BASE_URL}/api/auth/upload-image`, // Upload profile picture
+    UPLOAD_IMAGE: `/auth/upload-image`, // Upload profile picture
   },
 
   AI: {
-    GENERATE_QUESTIONS: `${BASE_URL}/api/ai/generate-questions`, // Generate interview questions and answers using Gemini
-    GENERATE_EXPLANATION: `${BASE_URL}/api/ai/generate-explanation`, // Generate concept explanation using Gemini
+    GENERATE_QUESTIONS: `/ai/generate-questions`, // Generate interview questions and answers using Gemini
+    GENERATE_EXPLANATION: `/ai/generate-explanation`, // Generate concept explanation using Gemini
   },
 
   SESSION: {
-    CREATE: `${BASE_URL}/api/sessions/create`, // Create a new interview session with questions
-    GET_ALL: `${BASE_URL}/api/sessions/my-sessions`, // Get all user sessions
-    GET_ONE: (id) => `${BASE_URL}/api/sessions/${id}`, // Get session details with questions
-    DELETE: (id) => `${BASE_URL}/api/sessions/${id}`, // Delete a session
+    CREATE: `/sessions/create`, // Create a new interview session with questions
+    GET_ALL: `/sessions/my-sessions`, // Get all user sessions
+    GET_ONE: (id) => `/sessions/${id}`, // Get session details with questions
+    DELETE: (id) => `/sessions/${id}`, // Delete a session
   },
 
   QUESTION: {
-    ADD_TO_SESSION: `${BASE_URL}/api/questions/add`, // Add more questions to a session
-    PIN: (id) => `${BASE_URL}/api/questions/${id}/pin`, // Pin or Unpin a question
-    UPDATE_NOTE: (id) => `${BASE_URL}/api/questions/${id}/note`, // Update/Add a note to a question
+    ADD_TO_SESSION: `/questions/add`, // Add more questions to a session
+    PIN: (id) => `/questions/${id}/pin`, // Pin or Unpin a question
+    UPDATE_NOTE: (id) => `/questions/${id}/note`, // Update/Add a note to a question
   },
 };
